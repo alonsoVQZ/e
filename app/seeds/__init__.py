d48eeb064dcea679addb4ec89809bab561fa098f
aoavzm <aoavzm@gmail.com>
2023-02-13 16:52:35 -0700

    rawr

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -21,11 +21,11 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_sellers()
-        undo_users()
-        undo_products()
-        undo_departments()
-        undo_medias()
+        # undo_sellers()
+        # undo_users()
+        # undo_products()
+        # undo_departments()
+        # undo_medias()
 
     # Add other seed functions here
     seed_sellers()
