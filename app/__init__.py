dd6b52095746dc84979b73d38afaa7a6afa46b54
aoavzm <aoavzm@gmail.com>
2023-02-13 17:20:27 -0700

    rawr

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect, generate_csrf
 from .models import db, User
 from .routes import api
 from .seeds import seed_commands
@@ -21,12 +23,34 @@ def load_account(id):
 app.config.from_object(Configuration)
 
 app.cli.add_command(seed_commands)
+
 app.register_blueprint(api, url_prefix='/api')
 
 
 db.init_app(app)
 Migrate(app, db)
 
+
+CORS(app)
+
+@app.before_request
+def https_redirect():
+    if os.environ.get('FLASK_ENV') == 'production':
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
+
+@app.after_request
+def inject_csrf_token(response):
+    response.set_cookie(
+        'csrf_token',
+        generate_csrf(),
+        secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
+        samesite='Strict' if os.environ.get(
+            'FLASK_ENV') == 'production' else None,
+        httponly=True)
+    return response
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
